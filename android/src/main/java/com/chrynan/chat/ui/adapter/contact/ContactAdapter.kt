f24b0d8f1a6ab569e8c499e979d0689faa73adf3
Chris <ckeenan@starry.com>
2020-01-01 23:39:26 -0500

    Created ContactAdapter

---
+++ android/src/main/java/com/chrynan/chat/ui/adapter/contact/ContactAdapter.kt
@@ -0,0 +1,41 @@
+package com.chrynan.chat.ui.adapter.contact
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chrynan.aaaah.Adapter
+import com.chrynan.aaaah.AdapterViewType
+import com.chrynan.aaaah.ViewType
+import com.chrynan.aaaah.from
+import com.chrynan.chat.R
+import com.chrynan.chat.di.Inject
+import com.chrynan.chat.ui.adapter.core.BaseAdapter
+import com.chrynan.chat.viewmodel.ContactItemViewModel
+import kotlinx.android.synthetic.main.adapter_contact.view.*
+
+@Adapter
+class ContactAdapter @Inject constructor(private val listener: ContactSelectedListener) :
+    BaseAdapter<ContactItemViewModel>() {
+
+    override val viewType = AdapterViewType.from(this::class.java)
+
+    override fun onHandlesItem(item: Any) = item is ContactItemViewModel
+
+    override fun onCreateView(parent: ViewGroup, viewType: ViewType): View =
+        LayoutInflater.from(parent.context).inflate(R.layout.adapter_contact, parent, false)
+
+    override fun onBindItem(view: View, item: ContactItemViewModel) {
+        view.apply {
+            contactUserImageView?.userImage = item.userImage
+            contactTitleTextView?.text = item.name
+            contactDescriptionTextView?.text = item.description
+            contactDescriptionTextView?.visibility = if (item.description == null) View.GONE else View.VISIBLE
+            setOnClickListener { listener.onContactSelected(item) }
+        }
+    }
+
+    interface ContactSelectedListener {
+
+        fun onContactSelected(item: ContactItemViewModel)
+    }
+}
---
+++ presentation/src/commonMain/kotlin/com/chrynan/chat/viewmodel/ContactItemViewModel.kt
@@ -0,0 +1,16 @@
+package com.chrynan.chat.viewmodel
+
+import com.chrynan.aaaah.asUniqueAdapterId
+import com.chrynan.chat.adapter.AdapterItem
+import com.chrynan.chat.model.ID
+import com.chrynan.chat.model.UserImage
+
+data class ContactItemViewModel(
+    val userID: ID,
+    val userImage: UserImage,
+    val name: String,
+    val description: String?
+) : AdapterItem {
+
+    override val uniqueAdapterId = userID.asUniqueAdapterId()
+}
