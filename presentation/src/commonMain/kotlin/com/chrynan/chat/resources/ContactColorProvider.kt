f3a97fede6ba2d6e9b9100e8b567216138ad9a1c
Chris <ckeenan@starry.com>
2020-01-06 13:32:56 -0500

    Created ContactColorProvider

---
+++ presentation/src/commonMain/kotlin/com/chrynan/chat/resources/ContactColorProvider.kt
@@ -0,0 +1,20 @@
+package com.chrynan.chat.resources
+
+import com.chrynan.chat.di.Inject
+import com.chrynan.chat.model.ColorInt
+
+class ContactColorProvider @Inject constructor(private val colors: Colors) {
+
+    private val contactColors by lazy {
+        listOf(
+            colors.accentOne,
+            colors.accentTwo,
+            colors.accentThree,
+            colors.accentFour,
+            colors.accentFive,
+            colors.accentSix
+        )
+    }
+
+    fun getFor(contact: Any): ColorInt = contactColors[contact.hashCode() % contactColors.size]
+}
